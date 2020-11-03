eb058dcad505aa702fd67dba207fb0210eb836e3
Christopher Hoffmann <choffman@adobe.com>
2020-11-03 14:10:10 -0700

    Init Xcode Project

---
+++ AEPTarget/AEPTarget/AEPTarget.h
@@ -0,0 +1,18 @@
+//
+//  AEPTarget.h
+//  AEPTarget
+//
+//  Created by Christopher Hoffman on 11/3/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AEPTarget.
+FOUNDATION_EXPORT double AEPTargetVersionNumber;
+
+//! Project version string for AEPTarget.
+FOUNDATION_EXPORT const unsigned char AEPTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AEPTarget/PublicHeader.h>
+
+
