9c881a0fb8e57080c134331b4e59b5b86f5ca87c
Yansong <yanyang@adobe.com>
2020-11-04 16:08:30 -0700

    update project settings

--- AEPTarget/Tests/AEPTargetTests.swift
+++ AEPTarget/Tests/AEPTargetTests.swift
@@ -27,10 +27,4 @@ class AEPTargetTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
